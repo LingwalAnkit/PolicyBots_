0bcab1577bc79ca1c999936f27dcbe86fde81197
Sanyam Pandey <sanyam071104@gmail.com>
2024-10-01 10:56:38 +0530

    color schemes and dark mode

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -195,9 +195,9 @@ const ProfilePage = () => {
   return (
     <div className="bg-gray-100 dark:bg-gray-700 min-h-screen">
       <header className="bg-white dark:bg-gray-800 pb-4 pt-1 flex justify-between items-center">
-        <div className="flex flex-col items-center pt-2 px-16">
-          <h1 className="dark:text-white">Policy Bots</h1>
-          <span className="ml-2 text-xs text-blue-600 font-semibold">HAR BOT HOGA INSURED</span>
+      <div className="flex flex-col items-center pt-2 px-16">
+          <h1 className=' dark:text-white'>Policy Bots</h1>
+          <span className="ml-2 text-xs text-blue-600 font-semibold dark:text-[#A9D6E5]">HAR BOT HOGA INSURED</span>
         </div>
         <ThemeToggle />
       </header>
@@ -205,40 +205,38 @@ const ProfilePage = () => {
       <main className="container p-0 pt-8">
         <div className="flex justify-between gap-8 -mx-16">
           {/* Sidebar */}
-          <div className="w-1/3">
+          <div className='w-1/3'>
             <div className="bg-white dark:bg-gray-600 rounded-lg shadow p-6 mb-6">
-              <h1 className="text-2xl font-bold mb-2 dark:text-white">
-                Hi, {user ? `${user.firstName} ${user.lastName}` : 'Guest'}! 👋
-              </h1>
-              <p className="text-gray-600 dark:text-gray-300">How have you been?</p>
-              
+              <h1 className="text-2xl font-bold mb-2 dark:text-[#F9FAFB]">Hi, Ankit! 👋</h1>
+              <p className="text-gray-600 dark:text-white">How have you been?</p>
+
               <nav className="mt-6 space-y-2">
-                <button className="w-full mb-2 text-left p-2 hover:bg-gray-100 dark:hover:bg-gray-500 rounded flex items-center">
+                <button className="w-full mb-2 text-left p-2 bg-blue-50 dark:bg-gray-500 text-blue-600  dark:text-[#F9FAFB] dark:font-semibold rounded flex items-center">
                   <Bell className="mr-2" size={20} />
                   Dashboard
                 </button>
-                <Link href="./AllPolicies">
-                  <button className="w-full mb-2 text-left p-2 hover:bg-gray-100 dark:hover:bg-gray-500 rounded flex items-center">
+                <Link href="./AllPolicies" >
+                  <button className="w-full mb-2 text-left p-2 hover:bg-gray-100 dark:hover:bg-gray-500 dark:text-[#F9FAFB] dark:font-semibold rounded flex items-center">
                     <User className="mr-2" size={20} />
-                    All Policies  
+                    All Policies
                   </button>
                 </Link>
                 <Link href="/policies">
-                  <button className="w-full text-left mb-2 p-2 hover:bg-gray-100 dark:hover:bg-gray-500 rounded flex items-center">
+                  <button className="w-full mb-2 text-left p-2 hover:bg-gray-100 dark:hover:bg-gray-500 dark:text-[#F9FAFB] dark:font-semibold rounded flex items-center">
                     <ClipboardList className="mr-2" size={20} />
                     Your policies
                   </button>
                 </Link>
-                <button className="w-full text-left mb-2 p-2 hover:bg-gray-100 dark:hover:bg-gray-500 rounded flex items-center">
+                <button className="w-full text-left mb-2 p-2 hover:bg-gray-100 dark:hover:bg-gray-500 dark:text-[#F9FAFB] dark:font-semibold rounded flex items-center">
                   <Info className="mr-2" size={20} />
                   Get help
                 </button>
-                <button className="w-full mb-2 text-left p-2 hover:bg-gray-100 dark:hover:bg-gray-500 rounded flex items-center">
+                <button className="w-full mb-2 text-left p-2 hover:bg-gray-100 dark:hover:bg-gray-500 dark:text-[#F9FAFB] dark:font-semibold rounded flex items-center">
                   <BadgeDollarSign className="mr-2" size={20} />
                   Your Transactions
                 </button>
                 <Link href="/profile">
-                  <button className="w-full mt-2 text-left p-2 bg-blue-50 dark:bg-gray-500 text-blue-600 dark:text-gray-800 rounded flex items-center">
+                  <button className="w-full mt-2 text-left p-2 hover:bg-gray-100 dark:hover:bg-gray-500 dark:text-[#F9FAFB] dark:font-semibold rounded flex items-center">
                     <User className="mr-2" size={20} />
                     Profile
                   </button>
